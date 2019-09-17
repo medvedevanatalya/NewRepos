9e43129b11037b2bc9f32908d7ba986f611ca574
МедведеваН <@405-07>
2019-09-17 18:56:09 +0600

    add Filters

--- WebApplication2/FiltersHelper.cs
+++ WebApplication2/FiltersHelper.cs
@@ -11,9 +11,15 @@ namespace WebApplication2
         public void OnException(ExceptionContext filterContext)
         {
             //if(!filterContext.ExceptionHandled && filterContext.Exception is ArgumentOutOfRangeException)
-            if(!filterContext.ExceptionHandled && filterContext.Exception is DivideByZeroException)
-                {
-                filterContext.Result = new RedirectResult("~/Views/Shared/Error.cshtml");
+            if (!filterContext.ExceptionHandled && filterContext.Exception is DivideByZeroException)
+            {
+                //filterContext.Result = new RedirectResult("~/Views/Shared/Error.cshtml");
+                //filterContext.ExceptionHandled = true;
+
+                filterContext.Result = ViewResult
+                    {
+                    ViewName = @"~/ Views / Shared / Error.cshtml";
+                };
                 filterContext.ExceptionHandled = true;
             }
         }
